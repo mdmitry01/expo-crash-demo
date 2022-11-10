dabc0335b311596755fd1dfcfb197651d0c0a1ce
Dmytro Merkulov <69001428+mdmitry01@users.noreply.github.com>
2022-11-10 10:55:31 +0200

    listen to the location updates to reproduce app crashes

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import * as TaskManager from 'expo-task-manager'
+import { startLocationUpdatesAsync, LocationAccuracy } from 'expo-location'
+
+const LOCATION_UPDATES_TASK_NAME = 'location-updates'
+
+TaskManager.defineTask(LOCATION_UPDATES_TASK_NAME, ({ data: { locations }, error }) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log('Received new locations', locations)
+})
+
+startLocationUpdatesAsync(LOCATION_UPDATES_TASK_NAME, {
+  accuracy: LocationAccuracy.High,
+  foregroundService: {
+    notificationTitle: 'foo',
+    notificationBody: 'bar',
+  },
+}).catch(console.error)
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +38,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ README.md
@@ -0,0 +1,17 @@
+## Minimal reproducible example for expo/expo#19873
+
+### How to start the app
+
+```
+yarn
+yarn android
+```
+
+### Steps to reproduce
+
+1. Grant the background location permissions to the app.
+2. Force stop the app and launch it again. The app should crash, it may require a few tries to actually happen.
+
+See the attached video.
+
+https://user-images.githubusercontent.com/69001428/201045068-6dff0721-55d0-4da3-8006-028b33313e58.mp4
